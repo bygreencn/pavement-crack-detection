d3a9ba65e70664033d7ade0d01918b85c998c69c
fan <fan@fan.tu.temple.edu>
2019-03-31 00:54:08 -0400

    Add existing file

---
+++ build/src/caffe/solver.d
@@ -0,0 +1,228 @@
+.build_release/src/caffe/solver.o: src/caffe/solver.cpp \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/hdf5.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5public.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5pubconf.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5version.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5api_adpt.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Apublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Ipublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Opublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Lpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Tpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5ACpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Cpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Dpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Epublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Epubgen.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Fpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Gpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5MMpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Ppublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Zpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5PLpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Rpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Spublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDcore.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDfamily.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDlog.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmpi.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmpio.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmulti.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDsec2.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDstdio.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDdirect.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/hdf5_hl.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/hdf5.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5DOpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5DSpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5LTpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5IMpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5TBpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5PTpublic.h \
+ include/caffe/net.hpp include/caffe/blob.hpp include/caffe/common.hpp \
+ include/caffe/util/device_alternate.hpp \
+ /usr/local/cuda/include/cublas_v2.h /usr/local/cuda/include/cublas_api.h \
+ /usr/local/cuda/include/driver_types.h \
+ /usr/local/cuda/include/host_defines.h \
+ /usr/local/cuda/include/cuComplex.h \
+ /usr/local/cuda/include/vector_types.h \
+ /usr/local/cuda/include/builtin_types.h \
+ /usr/local/cuda/include/device_types.h \
+ /usr/local/cuda/include/surface_types.h \
+ /usr/local/cuda/include/texture_types.h \
+ /usr/local/cuda/include/cuda_fp16.h \
+ /usr/local/cuda/include/library_types.h /usr/local/cuda/include/cuda.h \
+ /usr/local/cuda/include/cuda_runtime.h \
+ /usr/local/cuda/include/host_config.h \
+ /usr/local/cuda/include/channel_descriptor.h \
+ /usr/local/cuda/include/cuda_runtime_api.h \
+ /usr/local/cuda/include/cuda_device_runtime_api.h \
+ /usr/local/cuda/include/driver_functions.h \
+ /usr/local/cuda/include/vector_functions.h \
+ /usr/local/cuda/include/vector_functions.hpp \
+ /usr/local/cuda/include/curand.h /usr/local/cuda/include/driver_types.h \
+ .build_release/src/caffe/proto/caffe.pb.h include/caffe/syncedmem.hpp \
+ include/caffe/util/math_functions.hpp \
+ include/caffe/util/mkl_alternate.hpp include/caffe/layer.hpp \
+ include/caffe/layer_factory.hpp include/caffe/util/coords.hpp \
+ include/caffe/solver.hpp include/caffe/util/hdf5.hpp \
+ include/caffe/util/io.hpp include/caffe/util/upgrade_proto.hpp
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/hdf5.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5public.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5pubconf.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5version.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5api_adpt.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Apublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Ipublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Opublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Lpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Tpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5ACpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Cpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Dpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Epublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Epubgen.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Fpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Gpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5MMpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Ppublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Zpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5PLpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Rpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Spublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDcore.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDfamily.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDlog.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmpi.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmpio.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmulti.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDsec2.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDstdio.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDdirect.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/hdf5_hl.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/hdf5.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5DOpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5DSpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5LTpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5IMpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5TBpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5PTpublic.h:
+
+include/caffe/net.hpp:
+
+include/caffe/blob.hpp:
+
+include/caffe/common.hpp:
+
+include/caffe/util/device_alternate.hpp:
+
+/usr/local/cuda/include/cublas_v2.h:
+
+/usr/local/cuda/include/cublas_api.h:
+
+/usr/local/cuda/include/driver_types.h:
+
+/usr/local/cuda/include/host_defines.h:
+
+/usr/local/cuda/include/cuComplex.h:
+
+/usr/local/cuda/include/vector_types.h:
+
+/usr/local/cuda/include/builtin_types.h:
+
+/usr/local/cuda/include/device_types.h:
+
+/usr/local/cuda/include/surface_types.h:
+
+/usr/local/cuda/include/texture_types.h:
+
+/usr/local/cuda/include/cuda_fp16.h:
+
+/usr/local/cuda/include/library_types.h:
+
+/usr/local/cuda/include/cuda.h:
+
+/usr/local/cuda/include/cuda_runtime.h:
+
+/usr/local/cuda/include/host_config.h:
+
+/usr/local/cuda/include/channel_descriptor.h:
+
+/usr/local/cuda/include/cuda_runtime_api.h:
+
+/usr/local/cuda/include/cuda_device_runtime_api.h:
+
+/usr/local/cuda/include/driver_functions.h:
+
+/usr/local/cuda/include/vector_functions.h:
+
+/usr/local/cuda/include/vector_functions.hpp:
+
+/usr/local/cuda/include/curand.h:
+
+/usr/local/cuda/include/driver_types.h:
+
+.build_release/src/caffe/proto/caffe.pb.h:
+
+include/caffe/syncedmem.hpp:
+
+include/caffe/util/math_functions.hpp:
+
+include/caffe/util/mkl_alternate.hpp:
+
+include/caffe/layer.hpp:
+
+include/caffe/layer_factory.hpp:
+
+include/caffe/util/coords.hpp:
+
+include/caffe/solver.hpp:
+
+include/caffe/util/hdf5.hpp:
+
+include/caffe/util/io.hpp:
+
+include/caffe/util/upgrade_proto.hpp:
---
+++ build/tools/upgrade_net_proto_text.d
@@ -0,0 +1,238 @@
+.build_release/tools/upgrade_net_proto_text.o: \
+ tools/upgrade_net_proto_text.cpp include/caffe/caffe.hpp \
+ include/caffe/blob.hpp include/caffe/common.hpp \
+ include/caffe/util/device_alternate.hpp \
+ /usr/local/cuda/include/cublas_v2.h /usr/local/cuda/include/cublas_api.h \
+ /usr/local/cuda/include/driver_types.h \
+ /usr/local/cuda/include/host_defines.h \
+ /usr/local/cuda/include/cuComplex.h \
+ /usr/local/cuda/include/vector_types.h \
+ /usr/local/cuda/include/builtin_types.h \
+ /usr/local/cuda/include/device_types.h \
+ /usr/local/cuda/include/surface_types.h \
+ /usr/local/cuda/include/texture_types.h \
+ /usr/local/cuda/include/cuda_fp16.h \
+ /usr/local/cuda/include/library_types.h /usr/local/cuda/include/cuda.h \
+ /usr/local/cuda/include/cuda_runtime.h \
+ /usr/local/cuda/include/host_config.h \
+ /usr/local/cuda/include/channel_descriptor.h \
+ /usr/local/cuda/include/cuda_runtime_api.h \
+ /usr/local/cuda/include/cuda_device_runtime_api.h \
+ /usr/local/cuda/include/driver_functions.h \
+ /usr/local/cuda/include/vector_functions.h \
+ /usr/local/cuda/include/vector_functions.hpp \
+ /usr/local/cuda/include/curand.h /usr/local/cuda/include/driver_types.h \
+ .build_release/src/caffe/proto/caffe.pb.h include/caffe/syncedmem.hpp \
+ include/caffe/util/math_functions.hpp \
+ include/caffe/util/mkl_alternate.hpp include/caffe/filler.hpp \
+ include/caffe/layer.hpp include/caffe/layer_factory.hpp \
+ include/caffe/util/coords.hpp include/caffe/net.hpp \
+ include/caffe/parallel.hpp include/caffe/internal_thread.hpp \
+ include/caffe/solver.hpp include/caffe/util/blocking_queue.hpp \
+ include/caffe/util/benchmark.hpp include/caffe/util/io.hpp \
+ include/caffe/vision_layers.hpp include/caffe/common_layers.hpp \
+ include/caffe/data_layers.hpp \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/hdf5.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5public.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5pubconf.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5version.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5api_adpt.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Apublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Ipublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Opublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Lpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Tpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5ACpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Cpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Dpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Epublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Epubgen.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Fpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Gpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5MMpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Ppublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Zpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5PLpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Rpublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Spublic.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDcore.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDfamily.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDlog.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmpi.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmpio.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmulti.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDsec2.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDstdio.h \
+ /usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDdirect.h \
+ include/caffe/data_reader.hpp include/caffe/util/db.hpp \
+ include/caffe/data_transformer.hpp include/caffe/loss_layers.hpp \
+ include/caffe/neuron_layers.hpp include/caffe/util/upgrade_proto.hpp
+
+include/caffe/caffe.hpp:
+
+include/caffe/blob.hpp:
+
+include/caffe/common.hpp:
+
+include/caffe/util/device_alternate.hpp:
+
+/usr/local/cuda/include/cublas_v2.h:
+
+/usr/local/cuda/include/cublas_api.h:
+
+/usr/local/cuda/include/driver_types.h:
+
+/usr/local/cuda/include/host_defines.h:
+
+/usr/local/cuda/include/cuComplex.h:
+
+/usr/local/cuda/include/vector_types.h:
+
+/usr/local/cuda/include/builtin_types.h:
+
+/usr/local/cuda/include/device_types.h:
+
+/usr/local/cuda/include/surface_types.h:
+
+/usr/local/cuda/include/texture_types.h:
+
+/usr/local/cuda/include/cuda_fp16.h:
+
+/usr/local/cuda/include/library_types.h:
+
+/usr/local/cuda/include/cuda.h:
+
+/usr/local/cuda/include/cuda_runtime.h:
+
+/usr/local/cuda/include/host_config.h:
+
+/usr/local/cuda/include/channel_descriptor.h:
+
+/usr/local/cuda/include/cuda_runtime_api.h:
+
+/usr/local/cuda/include/cuda_device_runtime_api.h:
+
+/usr/local/cuda/include/driver_functions.h:
+
+/usr/local/cuda/include/vector_functions.h:
+
+/usr/local/cuda/include/vector_functions.hpp:
+
+/usr/local/cuda/include/curand.h:
+
+/usr/local/cuda/include/driver_types.h:
+
+.build_release/src/caffe/proto/caffe.pb.h:
+
+include/caffe/syncedmem.hpp:
+
+include/caffe/util/math_functions.hpp:
+
+include/caffe/util/mkl_alternate.hpp:
+
+include/caffe/filler.hpp:
+
+include/caffe/layer.hpp:
+
+include/caffe/layer_factory.hpp:
+
+include/caffe/util/coords.hpp:
+
+include/caffe/net.hpp:
+
+include/caffe/parallel.hpp:
+
+include/caffe/internal_thread.hpp:
+
+include/caffe/solver.hpp:
+
+include/caffe/util/blocking_queue.hpp:
+
+include/caffe/util/benchmark.hpp:
+
+include/caffe/util/io.hpp:
+
+include/caffe/vision_layers.hpp:
+
+include/caffe/common_layers.hpp:
+
+include/caffe/data_layers.hpp:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/hdf5.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5public.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5pubconf.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5version.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5api_adpt.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Apublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Ipublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Opublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Lpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Tpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5ACpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Cpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Dpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Epublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Epubgen.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Fpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Gpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5MMpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Ppublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Zpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5PLpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Rpublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5Spublic.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDcore.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDfamily.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDlog.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmpi.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmpio.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDmulti.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDsec2.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDstdio.h:
+
+/usr/lib/x86_64-linux-gnu/hdf5/serial/include/H5FDdirect.h:
+
+include/caffe/data_reader.hpp:
+
+include/caffe/util/db.hpp:
+
+include/caffe/data_transformer.hpp:
+
+include/caffe/loss_layers.hpp:
+
+include/caffe/neuron_layers.hpp:
+
+include/caffe/util/upgrade_proto.hpp:
